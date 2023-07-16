22e9ee7db77c87d709226df21fe17006eb4d5f58
Loki Astari <Loki.Astari@gmail.com>
2023-07-16 15:59:13 -0700

    Fix file name

--- src/ThorsCrypto/test/md5Test.cpp
+++ src/ThorsCrypto/test/md5Test.cpp
@@ -5,50 +5,50 @@
 using ThorsAnvil::Crypto::MD5;
 using ThorsAnvil::Crypto::Hash;
 
-TEST(md5, emptyOne)
+TEST(md5Test, emptyOne)
 {
     MD5 hash;
     Hash actual = hash.digest("");
     EXPECT_EQ(actual, Hash("d41d8cd98f00b204e9800998ecf8427e"));
 }
-TEST(md5, aOne)
+TEST(md5Test, aOne)
 {
     MD5 hash;
     Hash actual = hash.digest("a");
     EXPECT_EQ(actual, Hash("0cc175b9c0f1b6a831c399e269772661"));
 }
-TEST(md5, abcOne)
+TEST(md5Test, abcOne)
 {
     MD5 hash;
     Hash actual = hash.digest("abc");
     EXPECT_EQ(actual, Hash("900150983cd24fb0d6963f7d28e17f72"));
 }
-TEST(md5, messageDigetOne)
+TEST(md5Test, messageDigetOne)
 {
     MD5 hash;
     Hash actual = hash.digest("message digest");
     EXPECT_EQ(actual, Hash("f96b697d7cb7938d525a2f31aaf161d0"));
 }
-TEST(md5, a2zOne)
+TEST(md5Test, a2zOne)
 {
     MD5 hash;
     Hash actual = hash.digest("abcdefghijklmnopqrstuvwxyz");
     EXPECT_EQ(actual, Hash("c3fcd3d76192e4007dfb496cca67e13b"));
 }
-TEST(md5, A2Za2z029One)
+TEST(md5Test, A2Za2z029One)
 {
     MD5 hash;
     Hash actual = hash.digest("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789");
     EXPECT_EQ(actual, Hash("d174ab98d277d9f5a5611c2c9f419d9f"));
 }
-TEST(md5, eightOne)
+TEST(md5Test, eightOne)
 {
     MD5 hash;
     Hash actual = hash.digest("12345678901234567890123456789012345678901234567890123456789012345678901234567890");
     EXPECT_EQ(actual, Hash("57edf4a22be3c955ac49da2e2107b67a"));
 }
 
-TEST(md5, emptyTwo)
+TEST(md5Test, emptyTwo)
 {
     MD5 hash;
     hash.add("");
@@ -56,7 +56,7 @@ TEST(md5, emptyTwo)
     Hash actual = hash.hash();
     EXPECT_EQ(actual, Hash("d41d8cd98f00b204e9800998ecf8427e"));
 }
-TEST(md5, aTwo)
+TEST(md5Test, aTwo)
 {
     MD5 hash;
     hash.add("a");
@@ -64,7 +64,7 @@ TEST(md5, aTwo)
     Hash actual = hash.hash();
     EXPECT_EQ(actual, Hash("0cc175b9c0f1b6a831c399e269772661"));
 }
-TEST(md5, abcTwo)
+TEST(md5Test, abcTwo)
 {
     MD5 hash;
     hash.add("ab");
@@ -72,7 +72,7 @@ TEST(md5, abcTwo)
     Hash actual = hash.hash();
     EXPECT_EQ(actual, Hash("900150983cd24fb0d6963f7d28e17f72"));
 }
-TEST(md5, messageDigetTwo)
+TEST(md5Test, messageDigetTwo)
 {
     MD5 hash;
     hash.add("message di");
@@ -80,7 +80,7 @@ TEST(md5, messageDigetTwo)
     Hash actual = hash.hash();
     EXPECT_EQ(actual, Hash("f96b697d7cb7938d525a2f31aaf161d0"));
 }
-TEST(md5, a2zTwo)
+TEST(md5Test, a2zTwo)
 {
     MD5 hash;
     hash.add("abcdefghijklmnopqrstuvwx");
@@ -89,7 +89,7 @@ TEST(md5, a2zTwo)
     EXPECT_EQ(actual, Hash("c3fcd3d76192e4007dfb496cca67e13b"));
 }
 
-TEST(md5, A2Za2z029Two)
+TEST(md5Test, A2Za2z029Two)
 {
     MD5 hash;
     hash.add("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz");
@@ -97,7 +97,7 @@ TEST(md5, A2Za2z029Two)
     Hash actual = hash.hash();
     EXPECT_EQ(actual, Hash("d174ab98d277d9f5a5611c2c9f419d9f"));
 }
-TEST(md5, eightTwo)
+TEST(md5Test, eightTwo)
 {
     MD5 hash;
     hash.add("12");
